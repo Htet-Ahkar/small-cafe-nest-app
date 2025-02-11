36c791bbe0f805ce55843b54dd0b3e09c1df28fc
Xero <juliuscaesar.caesar37@gmail.com>
2025-02-11 18:44:56 +0700

    Add JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,10 +4,17 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
 
   async signup(dto: AuthDto) {
     try {
@@ -24,10 +31,8 @@ export class AuthService {
         },
       });
 
-      delete user.hash;
-
-      // return the saved user
-      return user;
+      // return the signinToken
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -55,8 +60,23 @@ export class AuthService {
     if (!isPasswordMatch)
       throw new ForbiddenException('Credentials incorrect.');
 
-    delete user.hash;
-    // return the user
-    return user;
+    // return the signinToken
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(userId: number, email: string) {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const token = await this.jwtService.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.configService.get('JWT_SECRET'),
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
